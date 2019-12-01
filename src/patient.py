72b45b522de29d33e55e29b9b3734d05cd35f700
matteo <m.facchetti.96@gmail.com>
2019-12-02 00:37:45 +0100

    Updated Patient class

--- src/patient.py
+++ src/patient.py
@@ -1,21 +1,49 @@
+import datetime
+
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
 
-from utils.data_utils import BlockChain
+from utils.data_utils import BlockChain, Block
 
 
 class Patient(BlockChain):
     """
     Patient object.
     """
-
     def __init__(self, name, *args, **kw):
         super(BlockChain, self).__init__(*args, **kw)
         self.name = name
         self.player = "patient"
         self.blocks = [self.get_genesis_block()]
-        self.private_key, self.public_key = self.get_keys()
+        self.get_keys()
+
+    def add_block(self, kind, data):
+        """
+        Add a new block to the chain.
+        """
+        self.blocks.append(Block(len(self.blocks), player=self.player, name=self.name,
+                                 timestamp=datetime.datetime.utcnow(), kind=kind, data=data,
+                                 previous_hash=self.blocks[len(self.blocks) - 1].hash))
+        # Encrypt blockchain
+        with open(f"../public_keys/{self.name}_public.txt", "rb") as key_file:
+            public_key = serialization.load_pem_public_key(
+                key_file.read(),
+                backend=default_backend()
+            )
+        prova = public_key.encrypt(
+            b"prova",
+            padding.OAEP(
+                mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                algorithm=hashes.SHA256(),
+                label=None
+            )
+        )
+        # TODO: Prevent from adding prescriptions that are not compatible with previous diseases
+        if kind == "prescription":
+            pass
 
     def get_keys(self):
         """
@@ -24,7 +52,7 @@ class Patient(BlockChain):
         # Generate keys
         private_key = rsa.generate_private_key(
             public_exponent=2*(len(self.blocks))+3,
-            key_size=512,
+            key_size=2048,
             backend=default_backend()
         )
         public_key = private_key.public_key()
@@ -38,7 +66,5 @@ class Patient(BlockChain):
         # Store keys
         with open(f'../private_keys/{self.name}_private.pem', 'wb') as f:
             f.write(private_key)
-        with open(f'../public_keys/{self.name}_public.pen', 'wb') as f:
+        with open(f'../public_keys/{self.name}_public.pem', 'wb') as f:
             f.write(public_key)
-
-        return private_key, public_key
--- src/utils/data_utils.py
+++ src/utils/data_utils.py
@@ -50,25 +50,13 @@ class BlockChain:
             return Block(index=0, player=self.player, name=self.name, timestamp=datetime.datetime.utcnow(),
                          kind="Genesis", data="Genesis", previous_hash="0" * 64)
 
-    def add_block(self, kind, data):
-        """
-        Add a new block to the chain.
-        """
-        if self.player == "patient":
-            self.blocks.append(Block(len(self.blocks), player=self.player, name=self.name,
-                                     timestamp=datetime.datetime.utcnow(), kind=kind, data=data,
-                                     previous_hash=self.blocks[len(self.blocks) - 1].hash))
-            # TODO: Prevent from adding prescriptions that are not compatible with previous diseases
-            if kind == "prescription":
-                pass
-
     def get_chain_size(self):
         """
         Return the length of the chain, excluding the genesis block.
         """
         return len(self.blocks) - 1
 
-    def verify(self, verbose=True):
+    def verify(self):
         """
         Verify data integrity:
 
@@ -80,18 +68,14 @@ class BlockChain:
         for i in range(1, len(self.blocks)):
             if self.blocks[i].index != i:
                 flag = False
-                if verbose:
-                    print(f'Wrong block index at block {i}.')
+                print(f'Wrong block index at block {i}.')
             if self.blocks[i - 1].hash != self.blocks[i].previous_hash:
                 flag = False
-                if verbose:
-                    print(f'Wrong previous hash at block {i}.')
+                print(f'Wrong previous hash at block {i}.')
             if self.blocks[i].hash != self.blocks[i].hashing():
                 flag = False
-                if verbose:
-                    print(f'Wrong hash at block {i}.')
+                print(f'Wrong hash at block {i}.')
             if self.blocks[i - 1].timestamp >= self.blocks[i].timestamp:
                 flag = False
-                if verbose:
-                    print(f'Backdating at block {i}.')
+                print(f'Backdating at block {i}.')
         return flag
